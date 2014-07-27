6b6d4fddc2a39469a544a13e18e9370ef7c902ee
rapier83 <rapier83@gmail.com>
2014-07-27 13:53:51 +0900

    Sumbit assigngemtn 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## check the matrix is invertible
     if(det(x) == 0) {
         message("The matrix is not invertible.")
     }
+    ## set m is empty
     m <- NULL
     set <- function(y) {
         x <<- y
@@ -25,11 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    #Get matrix from cache
     m <- x$getinv()
+    #If there is some matrix then don't calculte but just return from cache.
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    #Else, by using solve function calculate inverse matrix s
     data <- x$get()
     m <- solve(data,...)
     x$setinv(m)
